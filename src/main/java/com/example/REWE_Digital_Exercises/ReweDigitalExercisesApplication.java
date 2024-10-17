9ce0292ea81ecb8051a02f3e41a52f049508608e
iBreadd <124068827+iBreadd@users.noreply.github.com>
2024-10-17 10:53:30 +0300

    first commit and setup the project

---
+++ src/main/java/com/example/REWE_Digital_Exercises/ReweDigitalExercisesApplication.java
@@ -0,0 +1,13 @@
+package com.example.REWE_Digital_Exercises;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReweDigitalExercisesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReweDigitalExercisesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/REWE_Digital_Exercises/ReweDigitalExercisesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.REWE_Digital_Exercises;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReweDigitalExercisesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
